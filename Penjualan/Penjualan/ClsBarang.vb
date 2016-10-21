ae87b61931f6d200ef669d681e9624c9c3e002fd
hensh04 <the0hendri0droid@gmail.com>
2016-10-21 16:39:34 +0700

    ini nih class barang udah gw isi, ga ada yg lbh gegeh apa ?

--- Penjualan/Penjualan/ClsBarang.vb
+++ Penjualan/Penjualan/ClsBarang.vb
@@ -1,4 +1,5 @@
-﻿Public Class ClsBarang
+﻿Imports System.Data.Sql
+Public Class ClsBarang
     Private fkdbrg As String
     Private fnmbrg As String
     Private fsatuan As String
@@ -6,37 +7,37 @@
 
     Public Property pkdbrg As String
         Get
-
+            Return fkdbrg
         End Get
         Set(ByVal value As String)
-
+            fkdbrg = value
         End Set
     End Property
 
     Public Property pnmbrg As String
         Get
-
+            Return fnmbrg
         End Get
         Set(ByVal value As String)
-
+            fnmbrg = value
         End Set
     End Property
 
     Public Property psatuan As String
         Get
-
+            Return fsatuan
         End Get
         Set(ByVal value As String)
-
+            fsatuan = value
         End Set
     End Property
 
     Public Property pstok As Integer
         Get
-
+            Return fstok
         End Get
         Set(ByVal value As Integer)
-
+            fstok = value
         End Set
     End Property
 
